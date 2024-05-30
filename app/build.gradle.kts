9a82bfd49554e3efdf088af5c5513f88d7dc57f3
Vlad <vladyslav.balabanov@gmail.com>
2024-05-30 18:57:57 +0200

    Sync dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,26 +1,37 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.hilt)
+    id("com.google.devtools.ksp") version "1.9.0-1.0.12"
+    kotlin("kapt")
+    id("org.jetbrains.kotlin.plugin.parcelize")
+
 }
 
 android {
-    namespace = "com.example.thesweetspotmobile"
+    namespace = "com.example.sweetspot"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.thesweetspotmobile"
-        minSdk = 26
+        applicationId = "com.example.sweetspot"
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
@@ -30,14 +41,54 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.hilt.android)
+    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.accompanist.flowlayout)
+    implementation(libs.androidx.runtime.livedata)
+    implementation(libs.play.services.auth)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.material3)
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.kotlin.codegen)
+    implementation(libs.kotlinpoet)
+    implementation(libs.kotlinx.metadata.jvm)
+    implementation(libs.retrofit.converter.moshi)
+    implementation(libs.retrofit)
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
+    implementation(libs.android.facebook.login)
+    implementation(libs.facebook.android.sdk)
+    implementation(libs.java.jwt)
+    implementation(libs.jwtdecode)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
+
